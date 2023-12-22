230b7697b369a290faa6650991cab783b070e44f
Felipe Cerboncini <felipecerboncinix@gmail.com>
2023-12-22 17:08:38 -0300

    Add the WeakHashPredicate class

---
+++ src/main/java/com/cerbon/cerbons_api/general/data/WeakHashPredicate.java
@@ -0,0 +1,29 @@
+package com.cerbon.cerbons_api.general.data;
+
+import java.util.WeakHashMap;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+/**
+ * This class represents a weak hash predicate that tests objects of a generic type.
+ * It uses a weak hash map to store the conditions for each object and a supplier to generate these conditions.
+ * The conditions are weakly referenced, meaning they can be garbage collected when they are no longer in ordinary use.
+ *
+ * @param <T> the type of objects that this predicate can test
+ */
+public class WeakHashPredicate<T> implements Predicate<T> {
+    private final Supplier<Supplier<Boolean>> predicateFactory;
+    private final WeakHashMap<T, Supplier<Boolean>> conditionals = new WeakHashMap<>();
+
+    public WeakHashPredicate(Supplier<Supplier<Boolean>> predicateFactory) {
+        this.predicateFactory = predicateFactory;
+    }
+
+    @Override
+    public boolean test(T t) {
+        if (!conditionals.containsKey(t))
+            conditionals.put(t, predicateFactory.get());
+
+        return conditionals.get(t).get();
+    }
+}
