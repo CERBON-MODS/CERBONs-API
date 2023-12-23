eb6e8dfd43666f4f196706fde2b8cdaf30f0b747
Felipe Cerboncini <felipecerboncinix@gmail.com>
2023-12-23 00:38:55 -0300

    Add capability to track the player position

--- src/main/java/com/cerbon/cerbons_api/capability/CerbonsApiCapabilities.java
+++ src/main/java/com/cerbon/cerbons_api/capability/CerbonsApiCapabilities.java
@@ -1,12 +1,25 @@
 package com.cerbon.cerbons_api.capability;
 
+import com.cerbon.cerbons_api.api.general.data.HistoricalData;
 import com.cerbon.cerbons_api.capability.providers.LevelEventSchedulerProvider;
 import com.cerbon.cerbons_api.api.general.event.EventScheduler;
+import com.cerbon.cerbons_api.capability.providers.PlayerMoveHistoryProvider;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
+
+import java.util.Collections;
+import java.util.List;
 
 public class CerbonsApiCapabilities {
 
     public static EventScheduler getLevelEventScheduler(Level level) {
         return level.getCapability(LevelEventSchedulerProvider.EVENT_SCHEDULER).resolve().orElseThrow();
+    }
+
+    public static List<Vec3> getPlayerPositions(ServerPlayer player){
+        return player.getCapability(PlayerMoveHistoryProvider.HISTORICAL_DATA)
+                .map(HistoricalData::getAll)
+                .orElse(Collections.emptyList());
     }
 }
--- src/main/java/com/cerbon/cerbons_api/event/ForgeEvents.java
+++ src/main/java/com/cerbon/cerbons_api/event/ForgeEvents.java
@@ -3,8 +3,12 @@ package com.cerbon.cerbons_api.event;
 import com.cerbon.cerbons_api.CerbonsApi;
 import com.cerbon.cerbons_api.capability.providers.LevelEventSchedulerProvider;
 import com.cerbon.cerbons_api.api.general.event.EventScheduler;
+import com.cerbon.cerbons_api.capability.providers.PlayerMoveHistoryProvider;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -14,9 +18,30 @@ import net.minecraftforge.fml.common.Mod;
 public class ForgeEvents {
 
     @SubscribeEvent
+    public static void onAttachCapabilitiesPlayer(AttachCapabilitiesEvent<Entity> event) {
+        if (!(event.getObject() instanceof Player) || event.getObject().getCapability(PlayerMoveHistoryProvider.HISTORICAL_DATA).isPresent()) return;
+        event.addCapability(new ResourceLocation(CerbonsApi.MOD_ID, "player_move_history"), new PlayerMoveHistoryProvider());
+    }
+
+    @SubscribeEvent
     public static void onAttachCapabilitiesLevel(AttachCapabilitiesEvent<Level> event) {
         if (event.getObject() == null || event.getObject().getCapability(LevelEventSchedulerProvider.EVENT_SCHEDULER).isPresent()) return;
         event.addCapability(new ResourceLocation(CerbonsApi.MOD_ID, "event_scheduler"), new LevelEventSchedulerProvider());
+    }
+
+    @SubscribeEvent
+    public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
+        if (event.side.isClient()) return;
+        event.player.getCapability(PlayerMoveHistoryProvider.HISTORICAL_DATA).ifPresent(data -> {
+            Vec3 previousPosition = data.get(0);
+            Vec3 newPosition = event.player.position();
+
+            // Extremely fast movement in one tick is a sign of teleportation or dimension hopping, and thus we should clear history to avoid undefined behavior
+            if (previousPosition.distanceToSqr(newPosition) > 5)
+                data.clear();
+
+            data.add(newPosition);
+        });
     }
 
     @SubscribeEvent
