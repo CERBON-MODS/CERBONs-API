588edb59fc2deb9c3a1a7bb0ed80e621eacbcc86
Felipe <felipecerboncinix@gmail.com>
2023-12-26 22:41:42 -0300

    Add one more method to MobUtils

--- src/main/java/com/cerbon/cerbons_api/api/static_utilities/MobUtils.java
+++ src/main/java/com/cerbon/cerbons_api/api/static_utilities/MobUtils.java
@@ -1,6 +1,9 @@
 package com.cerbon.cerbons_api.api.static_utilities;
 
+import net.minecraft.world.Difficulty;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 
 public class MobUtils {
@@ -14,5 +17,10 @@ public class MobUtils {
 
     public static Vec3 lastRenderPos(Entity entity) {
         return new Vec3(entity.xOld, entity.yOld, entity.zOld);
+    }
+
+    public static void preventDespawnExceptPeaceful(LivingEntity mobEntity, Level level) {
+        if (level.getDifficulty() == Difficulty.PEACEFUL) mobEntity.discard();
+        else mobEntity.setNoActionTime(0);
     }
 }
