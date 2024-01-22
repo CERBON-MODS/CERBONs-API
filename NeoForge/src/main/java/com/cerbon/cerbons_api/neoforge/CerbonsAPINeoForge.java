6005198c768ae52d25bd57d10941789e94e7c40b
Felipe <felipecerboncinix@gmail.com>
2024-01-22 11:33:47 -0300

    Correctly registry network for neoForge

--- NeoForge/src/main/java/com/cerbon/cerbons_api/neoforge/CerbonsAPINeoForge.java
+++ NeoForge/src/main/java/com/cerbon/cerbons_api/neoforge/CerbonsAPINeoForge.java
@@ -1,13 +1,29 @@
 package com.cerbon.cerbons_api.neoforge;
 
 import com.cerbon.cerbons_api.CerbonsAPI;
+import com.cerbon.cerbons_api.api.network.CommonNetwork;
+import com.cerbon.cerbons_api.api.network.PacketRegistrationHandler;
+import com.cerbon.cerbons_api.api.network.data.Side;
+import com.cerbon.cerbons_api.neoforge.network.NeoForgeNetworkHandler;
 import com.cerbon.cerbons_api.util.Constants;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.fml.loading.FMLLoader;
 
 @Mod(Constants.MOD_ID)
 public class CerbonsAPINeoForge {
+    private final PacketRegistrationHandler handler;
 
-    public CerbonsAPINeoForge() {
+    public CerbonsAPINeoForge(IEventBus modEventBus) {
         CerbonsAPI.init();
+
+        modEventBus.addListener(this::commonSetupEvent);
+        handler = new NeoForgeNetworkHandler(FMLLoader.getDist().isClient() ? Side.CLIENT : Side.SERVER);
+        modEventBus.register(handler);
+    }
+
+    public void commonSetupEvent(FMLCommonSetupEvent event) {
+        new CommonNetwork(handler);
     }
 }
