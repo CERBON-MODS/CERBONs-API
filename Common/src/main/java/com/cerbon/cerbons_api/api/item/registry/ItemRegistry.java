b2154aec8ec558a584594d28079f28a4bd3e6f9b
felip <felipecerboncinix@gmail.com>
2025-04-29 19:47:03 -0300

    Create methods to register armor items

--- Common/src/main/java/com/cerbon/cerbons_api/api/item/registry/ItemRegistry.java
+++ Common/src/main/java/com/cerbon/cerbons_api/api/item/registry/ItemRegistry.java
@@ -4,11 +4,15 @@ import com.cerbon.cerbons_api.api.item.enums.ToolType;
 import com.cerbon.cerbons_api.api.registry.RegistryEntry;
 import com.cerbon.cerbons_api.api.registry.ResourcefulRegistries;
 import com.cerbon.cerbons_api.api.registry.ResourcefulRegistry;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableMap;
+import net.minecraft.core.Holder;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.block.Block;
 
 import java.util.Collection;
+import java.util.Map;
 import java.util.function.UnaryOperator;
 import java.util.stream.Stream;
 
@@ -17,6 +21,33 @@ public class ItemRegistry {
 
     public ItemRegistry(String modId) {
         this.itemRegistry = ResourcefulRegistries.create(BuiltInRegistries.ITEM, modId);
+    }
+
+    public Map<ArmorItem.Type, RegistryEntry<ArmorItem>> registerFullArmorSet(Holder<ArmorMaterial> material, int... durabilityFactors) {
+        return registerFullArmorSet(material, properties -> properties, durabilityFactors);
+    }
+
+    public Map<ArmorItem.Type, RegistryEntry<ArmorItem>> registerFullArmorSet(Holder<ArmorMaterial> material, UnaryOperator<Item.Properties> itemProperties, int... durabilityFactors) {
+        Preconditions.checkArgument(
+                durabilityFactors != null && durabilityFactors.length == 4,
+                "Expected durability array of length 4 (helmet, chest, legs, boots)"
+        );
+
+        return ImmutableMap.of(
+                ArmorItem.Type.HELMET, registerArmor(ArmorItem.Type.HELMET, material, itemProperties, durabilityFactors[0]),
+                ArmorItem.Type.CHESTPLATE, registerArmor(ArmorItem.Type.CHESTPLATE, material, itemProperties, durabilityFactors[1]),
+                ArmorItem.Type.LEGGINGS, registerArmor(ArmorItem.Type.LEGGINGS, material, itemProperties, durabilityFactors[2]),
+                ArmorItem.Type.BOOTS, registerArmor(ArmorItem.Type.BOOTS, material, itemProperties, durabilityFactors[3])
+        );
+    }
+
+    public RegistryEntry<ArmorItem> registerArmor(ArmorItem.Type armorType, Holder<ArmorMaterial> material, int durabilityFactor) {
+        return registerArmor(armorType, material, properties -> properties, durabilityFactor);
+    }
+
+    public RegistryEntry<ArmorItem> registerArmor(ArmorItem.Type armorType, Holder<ArmorMaterial> material, UnaryOperator<Item.Properties> itemProperties, int durabilityFactor) {
+        String materialName = material.getRegisteredName().split(":")[1];
+        return registerItem(new ArmorItem(material, armorType, itemProperties.apply(new Item.Properties().durability(armorType.getDurability(durabilityFactor)))), materialName + "_" + armorType.getSerializedName());
     }
 
     public RegistryEntry<TieredItem> registerTool(ToolType toolType, Tier tier, float attackDamage, float attackSpeed, String id) {
